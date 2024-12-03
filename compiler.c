69706fb41b94ce932ad79e63583da21b2df09bf1
tyagidevansh <devanshtyagi0607@gmail.com>
2024-12-03 23:50:09 +0530

    one token at a time

--- compiler.c
+++ compiler.c
@@ -6,4 +6,18 @@
 
 void compile(const char* source) {
   initScanner(source);
+  // temporary code to drive the scanner, as our compiler cannot yet ask the scanner for tokens
+  int line = -1;
+  for (;;) {
+    Token token = scanToken();
+    if (token.line != line) {
+      printf("%4d ", token.line);
+      line = token.line;
+    } else {
+      printf("  | ");
+    }
+    printf("%2d '%.*s'\n", token.type, token.length, token.start); // %.*s  allows us to pass the length of the string as an argument
+
+    if (token.type == TOKEN_EOF) break;
+  }
 }
