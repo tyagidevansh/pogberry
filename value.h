bf0a9a02696e98e18b1e9f879736fd502aee5494
tyagidevansh <devanshtyagi0607@gmail.com>
2024-12-23 17:01:09 +0530

    bools and nil

--- value.h
+++ value.h
@@ -3,7 +3,33 @@
 
 #include "common.h"
 
-typedef double Value;
+// the three number types currently supported
+typedef enum {
+  VAL_BOOL,
+  VAL_NIL,
+  VAL_NUMBER,
+} ValueType;
+
+typedef struct {
+  // one field to store the type
+  ValueType type;
+  // one field to store the union of all of the values it could store
+  union {
+    bool boolean;
+    double number;
+  } as;
+} Value;
+
+#define IS_BOOL(value)    ((value).type == VAL_BOOL)
+#define IS_NIL(value)     ((value).type == VAL_NIL)
+#define IS_NUMBER(value)  ((value).type == VAL_NUMBER)
+
+#define AS_BOOL(value)    ((value).as.boolean)
+#define AS_NUMBER(value)  ((value).as.number)
+
+#define BOOL_VAL(value)   ((Value){VAL_BOOL, {.boolean = value}})
+#define NIL_VAL           ((Value){VAL_NIL, {.number = 0}})
+#define NUMBER_VAL(value) ((Value){VAL_NUMBER, {.number = value}})
 
 //a constant pool of sorts, chunk stored opcode this one will store data
 typedef struct {
@@ -12,10 +38,11 @@ typedef struct {
   Value* values;
 } ValueArray;
 
+bool valuesEqual(Value a, Value b);
 //all the same functions as chunk as essentially the same task desired
 void initValueArray(ValueArray* array);
 void writeValueArray(ValueArray* array, Value value);
 void freeValueArray(ValueArray* array);
 void printValue(Value value);
 
-#endif // !clox_value_h
+#endif
