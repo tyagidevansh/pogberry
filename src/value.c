2fe7973c9ee540c505abd2a942c88aab699c042f
tyagidevansh <devanshtyagi0607@gmail.com>
2026-07-31 19:23:06 +0530

    fix list and string equality

--- src/value.c
+++ src/value.c
@@ -38,13 +38,59 @@ void printValue(Value value) {
   }
 }
 
-bool valuesEqual(Value a, Value b) {
-  if (a.type != b.type) return false;
-  switch (a.type) {
-    case VAL_BOOL:   return AS_BOOL(a) == AS_BOOL(b);
-    case VAL_NIL:    return true;
-    case VAL_NUMBER: return AS_NUMBER(a) == AS_NUMBER(b);
-    case VAL_OBJ:    return AS_OBJ(a) == AS_OBJ(b); // if two strings are the same, they will share the same memory as all the strings are interned
-    default:         return false;
+static bool stringsEqual(ObjString* left, ObjString* right) {
+  return left->length == right->length && memcmp(left->chars, right->chars, (size_t)left->length) == 0;
+}
+
+static bool listsEqual(ObjList* left, ObjList* right) {
+  if (left == right) return true;
+  if (left->items.count != right->items.count) {
+    return false;
+  }
+
+  for (int i = 0; i < left->items.count; i++) {
+    if (!valuesEqual(left->items.values[i], right->items.values[i])) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+static bool objectsEqual(Value left, Value right) {
+  Obj* leftObject = AS_OBJ(left);
+  Obj* rightObject = AS_OBJ(right);
+
+  if (leftObject == rightObject) return true;
+  if (leftObject->type != rightObject->type) return false;
+
+  switch (leftObject->type) {
+    case OBJ_STRING:
+      return stringsEqual((ObjString*)leftObject, (ObjString*)rightObject);
+
+    case OBJ_LIST:
+    return listsEqual((ObjList*)leftObject, (ObjList*)rightObject);
+    
+    default:
+      return false;
+  }
+}
+
+bool valuesEqual(Value left, Value right) {
+  if (left.type != right.type) return false;
+
+
+  switch (left.type) {
+    case VAL_BOOL:
+      return AS_BOOL(left) == AS_BOOL(right);
+    
+    case VAL_NUMBER:
+      return AS_NUMBER(left) == AS_NUMBER(right);
+
+    case VAL_NIL:
+      return true;
+    
+    case VAL_OBJ:
+    return objectsEqual(left, right);
   }
 }
